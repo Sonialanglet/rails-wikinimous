f5290adfb0dab28a99bb0837543285faa7dbf5a4
Sonialanglet <slanglet@loeildumarketing.com>
2017-11-21 17:32:44 +0100

    wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "cleaning seeds"
+Article.destroy_all
+
+puts 'Creating 10 fake articles...'
+10.times do
+  new_article = Article.new(
+    title:    Faker::HowIMetYourMother.catch_phrase,
+    content: Faker::HowIMetYourMother.quote,
+    created_at: Faker::Date.between(2.days.ago, Date.today),
+    updated_at:  Faker::Date.between(2.days.ago, Date.today),
+  )
+  new_article.save!
+end
+puts 'Finished!'
